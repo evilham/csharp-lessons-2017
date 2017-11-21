34ce756cb483aa2836ec9084bf93e69d0e7d3641
Evilham <github@evilham.com>
2017-11-21 14:32:44 +0100

    Added perimeter calculation.

--- A01_Euclidean_distance/A01_Euclidean_distance/Program.cs
+++ A01_Euclidean_distance/A01_Euclidean_distance/Program.cs
@@ -3,42 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace A01_Euclidean_distance
 {
     class Program
     {
+        static double readNumber()
+        {
+            string line = Console.ReadLine().Replace(",", ".");
+            return Convert.ToDouble(line, CultureInfo.InvariantCulture);
+        }
         static void Main(string[] args)
         {
             // Double: weil Realezahlen
-            double x1; double y1;
-            double x2; double y2;
+            Console.Write("How many vertices does your polygon have? ");
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            while (true)
+            double[] X_v = new double[n];
+            double[] Y_v = new double[n];
+
+            for (int i = 0; i < n; ++i)
             {
-                Console.Write("x1: ");
-                x1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("y1: ");
-                y1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("x2: ");
-                x2 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("y2: ");
-                y2 = Convert.ToDouble(Console.ReadLine());
-
-                double distanceInput = distanceTwoPoints(x1, y1, x2, y2);
-                Console.WriteLine(
-                    string.Format("Distance is: {0}", distanceInput));
-                Console.WriteLine();
+                Console.Write("X{0}: ", i + 1);
+                X_v[i] = readNumber();
+                Console.Write("Y{0}: ", i + 1);
+                Y_v[i] = readNumber();
             }
-       }
+
+            double d_total = 0.0;
+
+            for (int i = 0; i < n; ++i)
+            {
+                int j = (i + 1) % n;
+                double d_i = distanceTwoPoints(X_v[i], Y_v[i],
+                                               X_v[j], Y_v[j]);
+                d_total += d_i;
+            }
+            Console.Write("Perimeter: {0}", d_total);
+            Console.ReadLine();
+        }
         static double distanceTwoPoints(double x1, double y1, double x2, double y2)
         {
             double deltaX = (x2 - x1);
             double deltaY = (y2 - y1);
 
             double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
-
-            double distance2 = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
 
             return distance;
         }
