e06ea6ea971c7c6f064ee32c2237bae26ac03077
Evilham <github@evilham.com>
2017-11-28 18:47:50 +0100

    Added surface calculation and basic testing.

---
+++ A01_euclidean_distance/A01_euclidean_distance/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01_Euclidean_distance
+{
+    public class Point
+    {
+        public double X = 0.0;
+        public double Y = 0.0;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double distanceTo(Point p)
+        {
+            return distanceTwoPoints(this, p);
+        }
+
+        public static double distanceTwoPoints(Point p1, Point p2)
+        {
+            return distanceTwoPoints(p1.X, p1.Y, p2.X, p2.Y);
+        }
+
+        public static double distanceTwoPoints(double x1, double y1, double x2, double y2)
+        {
+            double deltaX = (x2 - x1);
+            double deltaY = (y2 - y1);
+
+            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+
+            return distance;
+        }
+    }
+}
